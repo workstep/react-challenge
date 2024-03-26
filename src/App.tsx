c5927d02f9dc7367a09f38a2143bc8d7332ba463
Chris Barmonde <chris@workstep.com>
2024-03-26 16:45:09 -0700

    Use vite

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+    return (
+        <>
+            <header className="header">My Pipeline</header>
+            <div>Hello world</div>
+        </>
+    );
+}
+
+export default App;
